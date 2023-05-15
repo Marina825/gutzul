dd05d72af29311863691d99bf0aa9b0b9e84968e
Marina Perfileva <masamokolova@gmail.com>
2023-05-15 22:11:03 +0300

    fix links

--- gutzul/src/App.js
+++ gutzul/src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
 import Appendix from "./Component/Appendix";
 import { AboutUs } from "./Component/AboutUs";
 import ProductPage from "./Component/ProductPage";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
+    <Router>
+    <Routes>
         <Route path="/gutzul" element={<Appendix />} />
         <Route index element={<Appendix />} />
         <Route path="aboutUs" element={<AboutUs />} />
-        <Route path="https://marina825.github.io/products" element={<ProductPage />} />
-      </Routes>
-    </BrowserRouter>
+        <Route path="/products" element={<ProductPage />} />
+        </Routes>
+    </Router>
   );
 }
 
